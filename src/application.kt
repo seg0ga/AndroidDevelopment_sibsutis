15e8f707f986b8a50cc6b4aa6f8f1b54642a914e
seg0ga <demin_s22@mail.ru>
2025-09-21 15:16:39 +0700

    second practice

--- src/application.kt
+++ src/application.kt
@@ -1,6 +1,7 @@
 import kotlin.random.Random
+import kotlin.concurrent.thread
 
-class Human {
+open class Human {
     var name: String = ""
         get() = field
         set(value){field =value}
@@ -39,21 +40,35 @@ class Human {
         currentSpeed= _speed
         println("Создан человек: $surname $name $second_name. Возраст: $age. Скорость $currentSpeed")}
 
-    fun move() {
+    open fun move() {
         val direction=Random.nextDouble(0.0, 2*Math.PI)
 
         x+=currentSpeed*Math.cos(direction)
         y+=currentSpeed*Math.sin(direction)
-        println("$name переместился на (${"%.2f".format(x)}; ${"%.2f".format(y)})")}
-}
+        println("$name перешел на (${"%.2f".format(x)}; ${"%.2f".format(y)})")}}
+
+class Driver:Human {
+    var angle:Double = 0.0
+        get()=field
+        set(value){field=value%(2*Math.PI)}
+
+    constructor(_name:String,_surname:String,_second:String,_age:Int,_speed: Double,_direction: Double):
+            super(_name, _surname, _second, _age, _speed) {
+        angle=_direction
+        println("Создан водитель: $surname $name $second_name. Направление: ${"%.2f".format(Math.toDegrees(angle))}°. Скорость: $currentSpeed")}
+
+    override fun move() {
+        x+=currentSpeed*Math.cos(angle)
+        y+=currentSpeed*Math.sin(angle)
+        println("$name переехал на (${"%.2f".format(x)}; ${"%.2f".format(y)})")}}
 
 fun main(){
-    val people = arrayOf(
+    val people=arrayOf(
         Human("Иван","Иванов","Иванович", 7, 1.1),
         Human("Сергей","Демин","Алексеевич", 42, 1.3),
         Human("Андрей","Кутенков","Алексеевич", 38, 1.5),
         Human("Михаил","Синица","Александрович", 17, 1.7),
-        Human("Никита","Криволапов","Алексеевич", 109, 1.9))
+        Driver("Никита","Криволапов","Алексеевич",109,15.0,Math.PI/4))
 
     val time_steps = 5
 
@@ -62,6 +77,11 @@ fun main(){
     for (step in 1..time_steps) {
         println("\nСекунда $step:")
         println("_____________________________________________")
+
+        val threads = mutableListOf<Thread>()
+
         for (person in people){
-            person.move()}}
-}
+            val thread=thread{
+                person.move()}
+            threads.add(thread)}
+        threads.forEach { it.join() }}}
